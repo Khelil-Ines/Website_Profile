8038ee8f0cb02c45cf7ac4e7dbb2a7a7d08b1c52
Khelil-Ines <96525392+Khelil-Ines@users.noreply.github.com>
2023-12-03 18:21:00 +0100

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,9 +5,9 @@ const Contact = () => {
         <div className='text'>
             <h1 className='p1'>Contact</h1>
             <p className='p'>My gmail:</p>
-            <p>khelilines14@gmail.com</p>
+            <p>******</p>
             <p className='p'>My linkedin account:</p>
-            <p>@KhelilInes</p>
+            <p>*******</p>
             <p className='p'>My phone number:</p>
             <p>+216 ** *** ***</p>
         </div>
