02bbdde6b488d282d973d173680908a1aaf6c20a
Khelil-Ines <96525392+Khelil-Ines@users.noreply.github.com>
2023-12-03 18:20:36 +0100

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ return(
 <>
 <p className='p1'>About me:</p>
 <p className='text'>
-       Hi, i'm Ines Khelil, and i love data analysis. <br/>
+       Hi, i'm ******, and i love data analysis. <br/>
        I'm a third-year Business Intelligence student at IHEC Carthage.
     </p>
    
